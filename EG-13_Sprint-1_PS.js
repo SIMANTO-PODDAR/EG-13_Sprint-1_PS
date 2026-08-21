e335c0ce708dea4a4ae516e3310a43dc8e49ccbd
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-08-21 11:59:41 +0600

    generateFibonacci

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -8,12 +8,23 @@ function isLeapYear(year) {
 
 
 
+function generateFibonacci(n) {
+    if (n === 0) return [];
+    if (n === 1) return [0];
+
+    const farr = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        farr.push(farr[i - 2] + farr[i - 1]);
+    }
+
+    return farr;
+}
 
 
 
-
-console.log(isLeapYear(2021))
-console.log(isLeapYear(2022))
-console.log(isLeapYear(2023))
-console.log(isLeapYear(2024))
-console.log(isLeapYear(2025))
+console.log('0', generateFibonacci(0))
+console.log('1', generateFibonacci(1))
+console.log('3', generateFibonacci(3))
+console.log('7', generateFibonacci(7))
+console.log('8', generateFibonacci(8))
