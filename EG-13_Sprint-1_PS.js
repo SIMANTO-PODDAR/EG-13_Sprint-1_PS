9ec2e5fe0ebdd4d567604a1e149d93e564d357a5
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-08-21 11:24:37 +0600

    isLeapYear

---
+++ EG-13_Sprint-1_PS.js
@@ -0,0 +1,19 @@
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+console.log(isLeapYear(2021))
+console.log(isLeapYear(2022))
+console.log(isLeapYear(2023))
+console.log(isLeapYear(2024))
+console.log(isLeapYear(2025))
